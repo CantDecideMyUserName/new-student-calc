2b8f2dafb0b0b941883d16742f03e803015b12c6
sailesh <saileshgiri25@gmail.com>
2025-11-27 19:26:31 +0545

    new adsense in layout and ads.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,8 +38,11 @@ export default function RootLayout({
             `
           }}
         />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6191764023643150"
-          crossOrigin="anonymous"></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6191764023643150"
+          crossOrigin="anonymous"
+        />
 
         {/* Clarity Analytics */}
         <script
